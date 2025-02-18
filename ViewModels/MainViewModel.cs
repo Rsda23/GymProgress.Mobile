85f251336ea47a8a0c6fb7043cc6b96da8bf2536
Rsda23 <rsda23.0@gmail.com>
2025-02-18 18:02:28 +0100

    feat: add main viewmodel

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymProgress.Mobile.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        private string buttonCreateSeance = "Créer";
+
+        [ObservableProperty]
+        private string titleSeance = "Séance";
+
+
+    }
+}
