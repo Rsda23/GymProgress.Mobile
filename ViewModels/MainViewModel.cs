cd0b8e8ba58df772869e83bc7a780719fda4d64c
Rsda23 <rsda23.0@gmail.com>
2025-02-19 16:37:20 +0100

    feat: bind stackloyout visibility to viewmodel

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,18 +1,34 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
 
 namespace GymProgress.Mobile.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+        public MainViewModel()
+        {
+            VisibleSeance();
+        }
+
         [ObservableProperty]
         private string buttonCreateSeance = "Créer";
 
         [ObservableProperty]
         private string titleSeance = "Séance";
+
+        [ObservableProperty]
+        private bool hasSeance;
+
+        [ObservableProperty]
+        private bool emptySeance;
+
+        private List<MainPage> fake = new List<MainPage>();
+
+        [RelayCommand]
+        private async Task VisibleSeance()
+        {
+            hasSeance = fake.Any();
+            emptySeance = !hasSeance;
+        }
     }
 }
