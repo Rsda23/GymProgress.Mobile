00b1a3e1595d0bfc66a64cf8f01d99106c338686
Rsda23 <rsda23.0@gmail.com>
2025-02-19 10:21:09 +0100

    chore: add extensions folder and move mvvm extensions

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using GymProgress.Mobile.ViewModels;
+
+namespace GymProgress.Mobile.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGymProgressViews(this IServiceCollection services)
+        {
+            services.AddTransient<MainPage>();
+            return services;
+        }
+
+        public static IServiceCollection AddGymProgressModels(this IServiceCollection services)
+        {
+            services.AddTransient<MainViewModel>();
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
