28d0f83ff57060e2cedd4f5d8f6e33f55138a3f4
Rsda23 <rsda23.0@gmail.com>
2025-04-04 18:01:19 +0200

    feat(add-exercice): display all exercices with checkbox for each (not yet linked)

--- ViewModels/Exercice/AddExerciceViewModel.cs
+++ ViewModels/Exercice/AddExerciceViewModel.cs
@@ -1,17 +1,45 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using GymProgress.Domain.Models;
+using GymProgress.Mobile.Interfaces;
+using System.Collections.ObjectModel;
 
 namespace GymProgress.Mobile.ViewModels
 {
     public partial class AddExerciceViewModel : ViewModelBase
     {
+        private readonly IExercicesService _exercicesService;
+        public AddExerciceViewModel(IExercicesService exercicesService)
+        {
+            _exercicesService = exercicesService;
+            DisplayExercice();
+        }
         [ObservableProperty]
         private string buttonCreateExerciceText = "Créer";
+
+        [ObservableProperty]
+        private bool isSelected;
+
+        [ObservableProperty]
+        private ObservableCollection<Exercice> exercices = new();
 
         [RelayCommand]
         private async Task ButtonCreateExercice()
         {
             await Shell.Current.GoToAsync("CreateExercicePage");
+        }
+
+        [RelayCommand]
+        private async Task DisplayExercice()
+        {
+            var exercices = await _exercicesService.GetAllExercice();
+            if (exercices != null)
+            {
+                foreach (var exercice in exercices)
+                {
+                    Exercices.Add(exercice);
+                }
+            }
         }
     }
 }
